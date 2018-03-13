4955fce8ac6f334b0acc2670f426f98a66bb9d32
Michael Mansi <mmansi18@gmail.com>
2018-03-12 20:30:29 -0700

    added finished html add js

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+   // Initialize Firebase
+   var config = {
+       apiKey: "AIzaSyA0jp-LR-XNd1-AnoJSttvWW7rY4XZQsT4",
+       authDomain: "employeedata-31bf9.firebaseapp.com",
+       databaseURL: "https://employeedata-31bf9.firebaseio.com",
+       projectId: "employeedata-31bf9",
+       storageBucket: "employeedata-31bf9.appspot.com",
+       messagingSenderId: "420064743945"
+   };
+   firebase.initializeApp(config);
+   firebase.initializeApp(config);
+   var database = firebase.database();
+   person = {
+       name: "",
+       role: "",
+       startdate: "",
+       rate: 0,
+   }
+   $("#submitButton").on("click", function () {
+       event.preventDefault();
+       person.name = $("#emName").val().trim();
+       person.role = $("#emRole").val().trim();
+       person.startdate = $("#emStartDate").val().trim();
+       person.rate = $("#emMonthRate").val().trim();
+       database.ref().child(person.name).update({
+           name: person.name,
+           role: person.role,
+           startdate: person.startdate,
+           rate: person.rate,
+       });
+   });
